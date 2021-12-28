ebe48d01e87320499071df9297269fe077b558e8
timwilliam <19308063+timwilliam@users.noreply.github.com>
2021-12-28 18:22:31 +0800

    Added .gitignore

---
+++ java-basic/src/StringType.java
@@ -0,0 +1,12 @@
+public class String {
+    public static void main(java.lang.String[] args) {
+        java.lang.String firstName = "Tim";
+        java.lang.String lastName = "William";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        java.lang.String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
